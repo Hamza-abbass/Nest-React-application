8ed806ff059f284be2e094d8786cfdc43266cf9f
Hamza-abbass <abbashamza59099@gmail.com>
2026-01-14 17:08:39 +0500

    Encryption and dycryption part

--- Seeding Nest/src/auth/auth.module.ts
+++ Seeding Nest/src/auth/auth.module.ts
@@ -7,12 +7,11 @@ import { RedisModule } from 'src/redis/redis.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { Rolee } from 'src/seed/role.entity';
-// import { EncryptionModule } from 'src/encryption-decryption/encryt.module';
-// EncryptionModule
+import { EncryptionModule } from 'src/encryption-decryption/encryt.module';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports:[UserModule,JwtModule,RedisModule,TypeOrmModule.forFeature([User,Rolee])]
+  imports:[UserModule,JwtModule,RedisModule,EncryptionModule,TypeOrmModule.forFeature([User,Rolee])]
 })
 export class AuthModule {}
